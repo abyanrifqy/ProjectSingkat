bb95b4a2a7e55e69d792a2bb976e8547a4a25280
abyanrifqy <66107885+abyanrifqy@users.noreply.github.com>
2021-06-09 23:35:45 +0700

    Add files via upload
    Code aplikasi admin

---
+++ app/admin_singkat/src/main/java/com/capstone/peradaban/admin_singkat/User.kt
@@ -0,0 +1,9 @@
+package com.capstone.peradaban.admin_singkat
+
+data class User (
+    val nik: String? = null,
+    val fullname: String? = null,
+    val jenisppks: String? = null,
+    val status: String? = null
+
+)
